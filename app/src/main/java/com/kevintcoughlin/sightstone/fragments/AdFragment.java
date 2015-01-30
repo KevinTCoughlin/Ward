9bc5c75b7deea893cc3a6cc4f16fd0f8e0a7d1e4
Kevin Coughlin <kevincoughlin@tumblr.com>
2015-01-29 23:52:28 -0500

    minor cleanup

--- app/src/main/java/com/kevintcoughlin/sightstone/fragments/AdFragment.java
+++ app/src/main/java/com/kevintcoughlin/sightstone/fragments/AdFragment.java
@@ -11,58 +11,42 @@ import com.google.android.gms.ads.AdView;
 import com.kevintcoughlin.sightstone.R;
 
 public class AdFragment extends Fragment {
-
     private AdView mAdView;
 
     public AdFragment() {
     }
 
-    @Override
-    public void onActivityCreated(Bundle bundle) {
+    @Override public void onActivityCreated(Bundle bundle) {
         super.onActivityCreated(bundle);
 
-        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
-        // values/strings.xml.
-        mAdView = (AdView) getView().findViewById(R.id.adView);
-
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("C6D397172C2598AF256CF30C6393FBFC")
                 .build();
 
-        // Start loading the ad in the background.
+        mAdView = (AdView) getView().findViewById(R.id.adView);
         mAdView.loadAd(adRequest);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_ad, container, false);
     }
 
-
-    @Override
-    public void onPause() {
+    @Override public void onPause() {
         if (mAdView != null) {
             mAdView.pause();
         }
         super.onPause();
     }
 
-    /** Called when returning to the activity */
-    @Override
-    public void onResume() {
+    @Override public void onResume() {
         super.onResume();
         if (mAdView != null) {
             mAdView.resume();
         }
     }
 
-    /** Called before the activity is destroyed */
-    @Override
-    public void onDestroy() {
+    @Override public void onDestroy() {
         if (mAdView != null) {
             mAdView.destroy();
         }
