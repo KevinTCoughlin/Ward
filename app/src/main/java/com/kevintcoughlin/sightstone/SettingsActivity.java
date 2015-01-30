8743012a2e7616b44a5d129b07ed1df8900f6313
Kevin Coughlin <kevincoughlin@tumblr.com>
2015-01-29 22:44:00 -0500

    region preference added.. developer site is down so waiting for that for constants

---
+++ app/src/main/java/com/kevintcoughlin/sightstone/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.kevintcoughlin.sightstone;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+}
